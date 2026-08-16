566e21bd0b59609644e8ed2fbc655766d4cd1587
Mihir Nagda <mihirnagda28@gmail.com>
2026-08-16 21:56:39 +0530

    fix(android): enable edge-to-edge status bar

--- android/app/src/main/java/com/afterthought/offset/MainActivity.java
+++ android/app/src/main/java/com/afterthought/offset/MainActivity.java
@@ -1,5 +1,28 @@
 package com.afterthought.offset;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            getWindow().setNavigationBarContrastEnforced(false);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+
+        WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView())
+                .setAppearanceLightStatusBars(false);
+    }
+}
