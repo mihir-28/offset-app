5db971cd2f588e259bdd372e76d167b307420fcf
Mihir Nagda <mihirnagda28@gmail.com>
2026-08-16 22:06:05 +0530

    fix(android): render app background behind status bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
     themeColor: "#09090B",
+    viewportFit: "cover",
     width: "device-width",
     initialScale: 1,
     maximumScale: 1,
